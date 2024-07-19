b07dfb79bbc6fc1db83960dbbb4bae7eaf04cd67
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-07-19 13:00:45 +0000

    fix

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ func main() {
 	router := gin.Default()
 
 	// Define routes
-	router.POST("/send-sms", controllers.SendSMS)
-	router.POST("/chat", controllers.Chat)
+	router.POST("/voice", controllers.HandleIncomingCall)
+	router.POST("/gather", controllers.GatherSpeech)
+	router.POST("/process-speech", controllers.ProcessSpeech)
 
 	// Start server
 	router.Run(":8080")
